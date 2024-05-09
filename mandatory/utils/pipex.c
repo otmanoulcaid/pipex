5b088163ef9d74e2f26702d1f4e46c2d41dcf297
otman oulcaid <57399454+otmanoulcaid@users.noreply.github.com>
2024-05-09 19:22:03 +0000

    version 1.1

--- mandatory/utils/pipex.c
+++ mandatory/utils/pipex.c
@@ -22,14 +22,15 @@ void	pipex(char **av, char **env, t_pipex *data)
 	if (!data->pid[0])
 		(close(data->pipe[0]), process(data->fd_in, data->pipe[1], av[1], env));
 	else
-		(close(data->pipe[1]), waitpid(data->pid[0], NULL, 0));
+		(close(data->pipe[1]));
 	data->pid[1] = fork();
 	if (data->pid[1] == -1)
 		ft_throw(strerror(errno));
 	if (!data->pid[1])
 		process(data->pipe[0], data->fd_out, av[2], env);
 	else
-		(close(data->pipe[1]), waitpid(data->pid[1], NULL, 0));
+		(close(data->pipe[1]), waitpid(data->pid[0], NULL, 0), \
+	waitpid(data->pid[1], NULL, 0));
 }
 
 void	process(int input, int output, char *av, char **env)
